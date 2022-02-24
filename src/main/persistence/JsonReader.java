9e7fa452b1a5ebda8ec2528878c3ef2441f27e12
jchow97 <jchow97@students.cs.ubc.ca>
2022-02-23 22:11:48 -0800

    Small additions to jsonReader class.

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -1,4 +1,15 @@
 package persistence;
 
+// Class is modeled after JsonSerializationDemo program provided in Phase 2 instructions.
+// Represents a reader that reads PlayerData from JSON data stored in file.
 public class JsonReader {
+    private String source;
+
+    // EFFECTS: constructs reader to read from source file
+    public JsonReader(String source) {
+        this.source = source;
+    }
+
+    // EFFECTS: reads player data from file and returns it;
+
 }
